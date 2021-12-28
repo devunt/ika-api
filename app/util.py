feecc480050186a649a1b73689721e4736e20b8f
finn <finn@dunamu.com>
2021-12-28 22:49:54 +0900

    닉네임 sanitization 로직 변경

--- app/util.py
+++ app/util.py
@@ -1,11 +1,2 @@
-forbidden_characters = [' ', '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '{', '}',
-                        '[', ']', '|', '\\', ':', ';', '"', '\'', '<', '>', ',', '.', '?', '/']
-
-
 def sanitize_nickname(nickname):
-    return ''.join(filter(is_acceptable_character, nickname))[:16]
-
-
-def is_acceptable_character(c):
-    o = ord(c)
-    return 65 <= o <= 90 or 97 <= o <= 122 or 48 <= o <= 57 or 44032 <= o <= 55203 or c in ['_', '-', '.']
+    return nickname.replace(' ', '_').replace('!', 'ǃ').replace('@', '＠')
